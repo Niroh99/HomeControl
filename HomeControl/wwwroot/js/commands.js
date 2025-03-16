21a71bdac792e0d07193f457045da79e45025c42
Niroh <Niroh@DESKTOP-CN5UCMD>
2025-03-16 17:04:51 +0100

    small changes

--- HomeControl/wwwroot/js/commands.js
+++ HomeControl/wwwroot/js/commands.js
@@ -1,5 +1,6 @@
 ﻿var smallDisplayCommandsToggleButton = document.getElementById("small-display-commands-toggle-button");
 var smallDisplayCommandsContainer = document.getElementById("small-display-commands-container");
+var smallDisplayCommandsOverlayButton = document.getElementById("small-display-commands-overlay-button");
 
 if (smallDisplayCommandsContainer.children.length == 0) {
     smallDisplayCommandsToggleButton.classList.add("hidden");
@@ -12,10 +13,12 @@ function toggleSmallCommandsDisplay() {
         smallDisplayCommandsToggleButton.isChecked = false;
 
         smallDisplayCommandsContainer.classList.add("hidden");
+        smallDisplayCommandsOverlayButton.classList.add("hidden");
     }
     else {
         smallDisplayCommandsToggleButton.isChecked = true;
 
         smallDisplayCommandsContainer.classList.remove("hidden");
+        smallDisplayCommandsOverlayButton.classList.remove("hidden");
     }
 }
